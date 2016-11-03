2b14eaa8f6133e7e6ce0a063a9e52b8ff44379fe
Zhouenze <ez242@cornell.edu>
2016-11-02 20:05:11 -0400

    add query

--- Samples/samples/testInput/queries.sql
+++ Samples/samples/testInput/queries.sql
@@ -1,15 +1,3 @@
-SELECT * FROM Sailors;
-SELECT Sailors.A FROM Sailors;
-SELECT Boats.F, Boats.D FROM Boats;
-SELECT Reserves.G, Reserves.H FROM Reserves;
-SELECT * FROM Sailors WHERE Sailors.B >= Sailors.C;
-SELECT Sailors.A FROM Sailors WHERE Sailors.B >= Sailors.C
-SELECT Sailors.A FROM Sailors WHERE Sailors.B >= Sailors.C AND Sailors.B < Sailors.C;
-SELECT * FROM Sailors, Reserves WHERE Sailors.A = Reserves.G;
-SELECT * FROM Sailors, Reserves, Boats WHERE Sailors.A = Reserves.G AND Reserves.H = Boats.D;
-SELECT * FROM Sailors, Reserves, Boats WHERE Sailors.A = Reserves.G AND Reserves.H = Boats.D AND Sailors.B < 150;
-SELECT DISTINCT * FROM Sailors;
-SELECT * FROM Sailors S1, Sailors S2 WHERE S1.A < S2.A;
-SELECT B.F, B.D FROM Boats B ORDER BY B.D;
-SELECT * FROM Sailors S, Reserves R, Boats B WHERE S.A = R.G AND R.H = B.D ORDER BY S.C;
-SELECT DISTINCT * FROM Sailors S, Reserves R, Boats B WHERE S.A = R.G AND R.H = B.D ORDER BY S.C;
+SELECT * FROM testData1, testData2 WHERE testData1.A = testData2.G;
+SELECT data1.A, data2.G, data1.B, data2.H FROM testData1 data1, testData2 data2 WHERE data1.A = data2.G AND data1.B = data2.H;
+SELECT DISTINCT * FROM testData1, testData2 WHERE testData1.A = testData2.G ORDER BY testData1.A;
